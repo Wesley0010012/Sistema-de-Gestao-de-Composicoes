c4518882daf9dfa9329b360d7ca985b9cb8202e4
Wesley0010012 <wglaurindo302@gmail.com>
2026-03-22 17:50:00 -0300

    feat: add Periods repository

---
+++ Backend/app/Modules/Composers/Core/Repositories/PeriodsRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Modules\Composers\Core\Repositories;
+
+use App\Modules\Shared\Core\Traits\Repositories\FindManyByMultiplesIdsTrait;
+
+abstract class PeriodsRepository
+{
+    use FindManyByMultiplesIdsTrait;
+}
