97d28e99d41a0a3025b6ace00e9cc06486e2386d
Wesley0010012 <wglaurindo302@gmail.com>
2026-03-29 18:08:14 -0300

    refactor: add AddTrait

--- Backend/app/Modules/Composers/Core/Repositories/ComposersRepository.php
+++ Backend/app/Modules/Composers/Core/Repositories/ComposersRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Modules\Composers\Core\Repositories;
 
+use App\Modules\Shared\Core\Traits\Repositories\AddTrait;
 use App\Modules\Shared\Core\Traits\Repositories\CountAllTrait;
 use App\Modules\Shared\Core\Traits\Repositories\FindAllTrait;
 use App\Modules\Shared\Core\Traits\Repositories\FindByIdTrait;
@@ -9,8 +10,7 @@ use App\Modules\Shared\Core\Traits\Services\DeleteByIdTrait;
 
 abstract class ComposersRepository
 {
-    use FindAllTrait, CountAllTrait, FindByIdTrait, DeleteByIdTrait;
-
+    use AddTrait, FindAllTrait, CountAllTrait, FindByIdTrait, DeleteByIdTrait;
 
     public abstract function existsByName(string $name): bool;
 
