6ad4314b5b62ed2ee8d1a5a4a1294ffcb25ea7b6
Wesley0010012 <wglaurindo302@gmail.com>
2026-03-29 18:08:32 -0300

    feat: add AddTrait

--- Backend/app/Modules/Composers/Core/Services/ComposersService.php
+++ Backend/app/Modules/Composers/Core/Services/ComposersService.php
@@ -8,6 +8,7 @@ use App\Modules\Composers\Core\Entities\Composer;
 use App\Modules\Composers\Core\Entities\Period;
 use App\Modules\Composers\Core\Repositories\ComposersRepository;
 use App\Modules\Shared\Core\Entities\AbstractEntity;
+use App\Modules\Shared\Core\Traits\Services\AddEntityTrait;
 use App\Modules\Shared\Core\Traits\Services\CountAllTrait;
 use App\Modules\Shared\Core\Traits\Services\DeleteByIdTrait;
 use App\Modules\Shared\Core\Traits\Services\EntityToDtoTrait;
@@ -18,13 +19,42 @@ use Exception;
 
 class ComposersService
 {
-    use EntityToDtoTrait, DeleteByIdTrait, FindAllTrait, FindEntityByIdTrait, FindAllPaginatedTrait, CountAllTrait;
+    use AddEntityTrait, EntityToDtoTrait, DeleteByIdTrait, FindAllTrait, FindEntityByIdTrait, FindAllPaginatedTrait, CountAllTrait;
 
     public function __construct(
         private readonly ComposersRepository $repository,
         private readonly PeriodsService $periodsService,
         private readonly NationalitiesService $nationalitiesService
     ) {}
+
+    protected function applyAddRules(AbstractEntity $entity): void
+    {
+        $composer = (fn($i): Composer => $i)($entity);
+
+        if ($this->repository->existsByName($composer->getName())) {
+            throw new Exception();
+        }
+    }
+
+    protected function addInputToEntity(mixed $addInput): AbstractEntity
+    {
+        $addComposerDto = (fn($i): AddComposerDto => $i)($addInput);
+
+        $nationality = $this->nationalitiesService->findById($addComposerDto->getNationalityId());
+
+        $periods = $this->periodsService->findManyByMultiplesIds($addComposerDto->getPeriodsIds());
+
+        $composer = new Composer(
+            $addComposerDto->getName(),
+            $addComposerDto->getPhotoPath(),
+            $addComposerDto->getBirthDate(),
+            $nationality,
+            $addComposerDto->getDeathDate(),
+            $periods,
+        );
+
+        return $composer;
+    }
 
     public function entityToDto(AbstractEntity $entity): mixed
     {
