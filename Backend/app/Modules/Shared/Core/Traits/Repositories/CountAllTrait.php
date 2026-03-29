03fc54c9ed367d5d655d0876a6e62f7c86c9a59e
Wesley0010012 <wglaurindo302@gmail.com>
2026-03-29 18:08:04 -0300

    refactor: remove unused imports

--- Backend/app/Modules/Shared/Core/Traits/Repositories/CountAllTrait.php
+++ Backend/app/Modules/Shared/Core/Traits/Repositories/CountAllTrait.php
@@ -2,8 +2,6 @@
 
 namespace App\Modules\Shared\Core\Traits\Repositories;
 
-use App\Modules\Shared\Core\Entities\AbstractEntity;
-
 trait CountAllTrait
 {
     abstract public function countAll(): int;
--- Backend/app/Modules/Shared/Core/Traits/Repositories/DeleteByIdTrait.php
+++ Backend/app/Modules/Shared/Core/Traits/Repositories/DeleteByIdTrait.php
@@ -2,8 +2,6 @@
 
 namespace App\Modules\Shared\Core\Traits\Repositories;
 
-use App\Modules\Shared\Core\Entities\AbstractEntity;
-
 trait DeleteByIdTrait
 {
     public abstract function deleteById(int $id): void;
