c0f521aa8e40ebf7d9334bb7082bd76db1f88d2f
Wesley0010012 <wglaurindo302@gmail.com>
2026-04-12 11:21:56 -0300

    feat: implementa repositório para Gêneros

---
+++ Backend/app/Modules/Works/Core/Repositories/GenresRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Modules\Works\Core\Repositories;
+
+use App\Modules\Shared\Core\Traits\Repositories\FindAllTrait;
+
+abstract class GenresRepository
+{
+    use FindAllTrait;
+}
