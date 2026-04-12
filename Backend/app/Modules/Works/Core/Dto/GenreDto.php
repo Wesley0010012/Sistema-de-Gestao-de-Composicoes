4a4c29356e546b945b60c6770bb4ff9bd6324eed
Wesley0010012 <wglaurindo302@gmail.com>
2026-04-12 11:25:37 -0300

    feat: implementa dto para Gênero

---
+++ Backend/app/Modules/Works/Core/Dto/GenreDto.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Modules\Works\Core\Dto;
+
+class GenreDto
+{
+    public int $id;
+    public string $name;
+    public string $description;
+
+    public function __construct(int $id, string $name, string $description)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+}
