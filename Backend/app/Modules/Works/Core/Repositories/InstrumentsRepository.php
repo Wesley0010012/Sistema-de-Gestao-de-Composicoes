e2bf2f5222b274c15fdbfc17f9a5b1a0fefbd84f
Wesley0010012 <wglaurindo302@gmail.com>
2026-04-12 11:22:31 -0300

    feat: implementa repositório para instrumentos

---
+++ Backend/app/Modules/Works/Core/Repositories/InstrumentsRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Modules\Works\Core\Repositories;
+
+use App\Modules\Shared\Core\Traits\Repositories\FindAllTrait;
+
+abstract class InstrumentsRepository
+{
+    use FindAllTrait;
+}
