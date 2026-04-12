d2aa39dc2008dc7fefcc22292dfb884ea294f791
Wesley0010012 <wglaurindo302@gmail.com>
2026-04-12 10:35:28 -0300

    feat: inclui protocolos para FindAllPaginated e Update

--- Backend/app/Modules/Composers/Core/Repositories/ComposersRepository.php
+++ Backend/app/Modules/Composers/Core/Repositories/ComposersRepository.php
@@ -4,13 +4,15 @@ namespace App\Modules\Composers\Core\Repositories;
 
 use App\Modules\Shared\Core\Traits\Repositories\AddTrait;
 use App\Modules\Shared\Core\Traits\Repositories\CountAllTrait;
+use App\Modules\Shared\Core\Traits\Repositories\FindAllPaginatedTrait;
 use App\Modules\Shared\Core\Traits\Repositories\FindAllTrait;
 use App\Modules\Shared\Core\Traits\Repositories\FindByIdTrait;
+use App\Modules\Shared\Core\Traits\Repositories\UpdateTrait;
 use App\Modules\Shared\Core\Traits\Services\DeleteByIdTrait;
 
 abstract class ComposersRepository
 {
-    use AddTrait, FindAllTrait, CountAllTrait, FindByIdTrait, DeleteByIdTrait;
+    use FindAllPaginatedTrait, UpdateTrait, AddTrait, FindAllTrait, CountAllTrait, FindByIdTrait, DeleteByIdTrait;
 
     public abstract function existsByName(string $name): bool;
 
