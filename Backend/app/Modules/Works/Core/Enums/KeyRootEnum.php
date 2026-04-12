b6015dbd60660e31bb353eb0243efe464d809334
Wesley0010012 <wglaurindo302@gmail.com>
2026-04-12 11:15:45 -0300

    feat: inclui tipos de notas base de composição

---
+++ Backend/app/Modules/Works/Core/Enums/KeyRootEnum.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Modules\Works\Core\Enums;
+
+enum KeyRootEnum: string
+{
+    case C = 'C';
+    case C_SHARP = 'C#';
+    case D_FLAT = 'Db';
+
+    case D = 'D';
+    case D_SHARP = 'D#';
+    case E_FLAT = 'Eb';
+
+    case E = 'E';
+
+    case F = 'F';
+    case F_SHARP = 'F#';
+    case G_FLAT = 'Gb';
+
+    case G = 'G';
+    case G_SHARP = 'G#';
+    case A_FLAT = 'Ab';
+
+    case A = 'A';
+    case A_SHARP = 'A#';
+    case B_FLAT = 'Bb';
+
+    case B = 'B';
+}
