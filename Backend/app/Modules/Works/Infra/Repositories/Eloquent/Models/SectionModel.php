4cc50b6e512f2dc61b5b313df641e6bb40f8aa4d
Wesley0010012 <wglaurindo302@gmail.com>
2026-04-26 21:44:05 -0300

    feat: implementa model Eloquent para Section

---
+++ Backend/app/Modules/Works/Infra/Repositories/Eloquent/Models/SectionModel.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Modules\Works\Infra\Repositories\Eloquent\Models;
+
+use App\Modules\Shared\Core\Entities\AbstractEntity;
+use App\Modules\Shared\Infra\Repositories\Eloquent\Models\EloquentEntity;
+use App\Modules\Works\Core\Entities\Section;
+use App\Modules\Works\Core\Enums\KeyModeEnum;
+use App\Modules\Works\Core\Enums\KeyRootEnum;
+use App\Modules\Works\Core\Vo\Key;
+use DateTime;
+
+class SectionModel extends EloquentEntity
+{
+    protected $table = 'sections';
+
+    protected $fillable = [
+        'work_id',
+        'key_root',
+        'key_mode',
+        'active',
+    ];
+
+    protected $casts = [
+        'active' => 'boolean',
+    ];
+
+    public function scores()
+    {
+        return $this->hasMany(ScoreModel::class, 'section_id');
+    }
+
+    public function toEntity(): AbstractEntity
+    {
+        $scores = $this->scores
+            ? $this->scores->map(fn($s) => $s->toEntity())->toArray()
+            : [];
+
+        return new Section(
+            key: new Key(KeyRootEnum::from($this->key_root), KeyModeEnum::from($this->key_mode)),
+            scores: $scores,
+            id: $this->id,
+            createdAt: new DateTime($this->created_at),
+            updatedAt: new DateTime($this->updated_at),
+            active: $this->active
+        );
+    }
+}
