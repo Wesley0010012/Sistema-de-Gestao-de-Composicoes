ca924e92297b8bbd73dec9c3e2717d9d68cbc81b
Wesley0010012 <wglaurindo302@gmail.com>
2026-05-14 22:57:58 -0300

    feat: configura rotas e responsividade do frontend

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/mobile.css";
+import { NationalitiesContextProvider } from "./contexts/NationalititesContext.tsx";
+import { TotalComposersContextProvider } from "./contexts/TotalComposersContext.tsx";
+import { PeriodsContextProvider } from "./contexts/PeriodsContext.tsx";
+import { RecentComposersContextProvider } from "./contexts/RecentComposersContext.tsx";
+import { ComposersPageContextProvider } from "./contexts/ComposersPageContext.tsx";
+import { PageProvider } from "./contexts/PageContextProvider.tsx";
+import { SelectedNationalityContextProvider } from "./contexts/SelectedNationalityContext.tsx";
+import { SelectedPeriodContextProvider } from "./contexts/SelectedPeriodContext .tsx";
+import { SearchBarProvider } from "./contexts/SeachBarContext.tsx";
+import { ConfirmProvider } from "./contexts/ConfirmContext.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ConfirmProvider>
+      <SearchBarProvider>
+        <NationalitiesContextProvider>
+          <PeriodsContextProvider>
+            <TotalComposersContextProvider>
+              <RecentComposersContextProvider>
+                <PageProvider>
+                  <SelectedNationalityContextProvider>
+                    <SelectedPeriodContextProvider>
+                      <ComposersPageContextProvider>
+                        <App />
+                      </ComposersPageContextProvider>
+                    </SelectedPeriodContextProvider>
+                  </SelectedNationalityContextProvider>
+                </PageProvider>
+              </RecentComposersContextProvider>
+            </TotalComposersContextProvider>
+          </PeriodsContextProvider>
+        </NationalitiesContextProvider>
+      </SearchBarProvider>
+    </ConfirmProvider>
   </StrictMode>,
-)
+);
