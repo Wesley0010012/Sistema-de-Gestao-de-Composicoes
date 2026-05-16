826a991658a2a554efa02068b44a9863b828ef0d
Wesley0010012 <wglaurindo302@gmail.com>
2026-05-15 21:13:33 -0300

    feat: add Eloquent Users Repository driver

---
+++ Backend/app/Modules/Auth/Infra/Repositories/Eloquent/EloquentUsersRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Modules\Auth\Infra\Repositories\Eloquent;
+
+use App\Modules\Auth\Core\Entities\User;
+use App\Modules\Auth\Core\Repositories\UsersRepository;
+use App\Modules\Auth\Infra\Repositories\Eloquent\Models\UserModel;
+use App\Modules\Shared\Infra\Repositories\Eloquent\Models\EloquentEntity;
+use Illuminate\Support\Facades\Schema;
+
+class EloquentUsersRepository extends UsersRepository
+{
+    public function findByEmail(string $email): ?User
+    {
+        $query = UserModel::where('email', $email);
+
+        if (Schema::hasColumn('users', 'active')) {
+            $query->where('active', true);
+        }
+
+        $model = $query->first();
+
+        if (!$model) {
+            return null;
+        }
+
+        return ((fn($i): EloquentEntity => $i)($model))->toEntity();
+    }
+}
---
+++ Backend/app/Modules/Auth/Infra/Repositories/Eloquent/Models/UserModel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Modules\Auth\Infra\Repositories\Eloquent\Models;
+
+use App\Modules\Auth\Core\Entities\User;
+use DateTime;
+use Illuminate\Database\Eloquent\Model;
+
+class UserModel extends Model
+{
+    protected $table = 'users';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'active',
+    ];
+
+    protected $casts = [
+        'active' => 'boolean',
+    ];
+
+    public function toEntity(): User
+    {
+        return new User(
+            name: $this->name,
+            email: $this->email,
+            password: $this->password,
+            id: $this->id,
+            createdAt: new DateTime($this->created_at),
+            updatedAt: new DateTime($this->updated_at),
+            active: $this->active ?? true
+        );
+    }
+}
